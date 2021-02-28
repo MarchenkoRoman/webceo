25530d7860fa42b3aa2a5107f07328fc8c362d48
Blayne <roman.u.marchenko@gmail.com>
2021-02-28 19:45:24 +0200

    test '/' ok

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Item, Employee, Sale
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['item_name', 'price', 'quantity', 'available']
+    list_filter = ['available']
+    list_editable = ['price', 'quantity', 'available']
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['employee_name']
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['item',  'created_date']
+
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from annoying.decorators import render_to
+from . models import Item
 
-# Create your views here.
+
+@render_to('base.html')
+def item_list(request):
+    items = Item.objects.all()
+    return {'items': items}
