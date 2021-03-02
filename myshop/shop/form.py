18eda674e3e47815c415e85ce4a59a93fdaa4a99
Blayne <roman.u.marchenko@gmail.com>
2021-03-02 22:44:42 +0200

    test last task and a little bit css

--- myshop/shop/form.py
+++ myshop/shop/form.py
@@ -5,6 +5,5 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 100)]
 
 
 class CreateSaleForm(forms.Form):
-    employee = forms.ModelChoiceField(queryset=Employee.objects.all())
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
-                                      coerce=int)
+    employee = forms.ModelChoiceField(queryset=Employee.objects.all(), empty_label="Choose Employee")
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
